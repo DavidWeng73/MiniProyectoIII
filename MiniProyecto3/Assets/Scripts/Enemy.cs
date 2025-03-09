e1276a0533a37f22479ecf15dae2255c286a4edc
DavidYdev <159245014+DavidYdev@users.noreply.github.com>
2025-03-09 20:53:57 +0100

    EnemyAI_FreezeProjectile

--- MiniProyecto3/Assets/Scripts/Enemy.cs
+++ MiniProyecto3/Assets/Scripts/Enemy.cs
@@ -1,22 +1,72 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    
     public int health = 3;
+    public float freezeTime = 2f; 
+    private bool isFrozen = false;
+    private Rigidbody rb;
+    private EnemyAI enemyAI; 
+    private UnityEngine.AI.NavMeshAgent navAgent; 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        enemyAI = GetComponent<EnemyAI>();
+        navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
 
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        Debug.Log("Enemigo golpeado, vida restante: " + health);
-
-        if (health <= 0)
+        if (!isFrozen)
         {
-            Die();
+            health -= damage;
+            Debug.Log("Enemigo golpeado, vida restante: " + health);
+
+            StartCoroutine(FreezeEnemy());
+
+            if (health <= 0)
+            {
+                Die();
+            }
         }
     }
 
-    private void Die()  
+    private IEnumerator FreezeEnemy()
+    {
+        isFrozen = true;
+        if (navAgent != null)
+        {
+            navAgent.isStopped = true;
+        }
+
+        // Pausar la IA
+        if (enemyAI != null)
+        {
+            enemyAI.enabled = false;
+        }
+
+        Debug.Log("Enemigo congelado por " + freezeTime + " segundos");
+
+        yield return new WaitForSeconds(freezeTime);
+
+        if (navAgent != null)
+        {
+            navAgent.isStopped = false;
+        }
+
+        if (enemyAI != null)
+        {
+            enemyAI.enabled = true;
+        }
+
+        isFrozen = false;
+        Debug.Log("Enemigo descongelado");
+    }
+
+    private void Die()
     {
         Debug.Log("Enemigo eliminado");
         Destroy(gameObject);
